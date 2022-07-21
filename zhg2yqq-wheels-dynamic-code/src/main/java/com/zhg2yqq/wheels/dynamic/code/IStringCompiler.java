d6ed87d28a3dcc4982110b6a8beca00fbb145c09
zhg2yqq <875452220@qq.com>
2022-07-21 16:03:27 +0800

    fixed:修复打包后自带编译工具包无法正常加载问题 changed:修改说明文档

--- zhg2yqq-wheels-dynamic-code/src/main/java/com/zhg2yqq/wheels/dynamic/code/IStringCompiler.java
+++ zhg2yqq-wheels-dynamic-code/src/main/java/com/zhg2yqq/wheels/dynamic/code/IStringCompiler.java
@@ -4,6 +4,8 @@
  */
 package com.zhg2yqq.wheels.dynamic.code;
 
+import javax.tools.JavaCompiler;
+
 import com.zhg2yqq.wheels.dynamic.code.dto.CalTimeDTO;
 import com.zhg2yqq.wheels.dynamic.code.dto.CompileResult;
 import com.zhg2yqq.wheels.dynamic.code.exception.CompileException;
@@ -26,4 +28,10 @@ public interface IStringCompiler {
      */
     CompileResult compile(String fullClassName, String sourceCode, CalTimeDTO calTime)
         throws CompileException;
+    
+    /**
+     * 获取编译器
+     * @return
+     */
+    JavaCompiler getCompiler();
 }
